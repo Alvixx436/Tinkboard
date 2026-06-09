7b2c9bb61035763311eb890c7cf4f726b6ecb7c3
Alvin Gerolao <gerolaoalvin436@gmail.com>
2026-06-09 18:38:12 +0800

    update frontends finals

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const BASE_URL =
   import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
-
+console.log("Base URL:", BASE_URL);
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
   headers: {
