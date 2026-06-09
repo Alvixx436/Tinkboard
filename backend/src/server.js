51766e47e8fbc9fb5f4e4118c5b66e0da2015bc3
Alvin Gerolao <gerolaoalvin436@gmail.com>
2026-06-09 17:35:04 +0800

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,26 +1,38 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 import connectDB from "./config/db.js";
 import notesRoutes from "./routes/notesRoutes.js";
 import knowledgeRoutes from "./routes/knowledgeRoute.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import aiRoutes from "./routes/aiRoutes.js";
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-// Middleware to parse JSON bodies
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  }),
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    }),
+  );
+}
 app.use(express.json());
 app.use(rateLimiter);
+
 app.use("/api/notes", notesRoutes);
 app.use("/api/ai", aiRoutes);
 app.use("/api/knowledge", knowledgeRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
+  });
+}
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
